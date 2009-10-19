869d73a320cee4f597387d6bb2c44bc736f99f5e
Kostas Karachalios <kostas.karachalios@me.com>
2009-10-19 13:29:38 +0300

    fixed some bugs + removed unused .js files

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,9 +1,13 @@
-function moveCarousel(position){
+function moveCarousel(position, auto){
     // find the new seats
     currentCarouselPosition = newCarouselPosition;
     newCarouselPosition = carouselSeats.indexOf($$('#carousel .seat.'+position)[0]);
 
     repositionCarousel(position);
+    
+    if(!auto){
+        initRotation(5000);
+    };
 };
 
 function repositionCarousel(position){
@@ -91,22 +95,22 @@ function moveSeat(seat, position){
             break;
     };
     
-    var offSize = 'width: 60px; margin-left: -30px;';
+    var offSize = 'width: '+carouselCenterWidth/4+'px; margin-left: -'+carouselCenterWidth/8+'px; font-size: 0.25em;';
     
-    var offPos = 'left:';
-    var onPos = 'left:';
+    var offPos = 'bottom: 120px; left:';
+    var onPos = 'bottom: 80px; left:';
     if(position == 'left'){
-        offPos += '-20%;';
-        onPos += '20%;';
+        offPos += '-'+carouselPadding+'%;';
+        onPos += carouselPadding+'%;';
     }else{
-        offPos += '120%;';
-        onPos += '80%;';
+        offPos += 100+carouselPadding+'%;';
+        onPos += 100-carouselPadding+'%;';
     };
     
     if(currentCarouselPosition == null){
         // position and scale down
         if(position == 'center'){
-            seat.setStyle('width: 80px; left: 50%; margin-left: -40px;');
+            seat.setStyle('bottom: 60px; width: '+carouselCenterWidth/2+'px; left: 50%; margin-left: -'+carouselCenterWidth/4+'px; font-size: 0.5em;');
         }else{
             seat.setStyle(offSize + onPos);
         };
@@ -117,28 +121,40 @@ function moveSeat(seat, position){
     };
 
     if(position == 'center'){
+        css += 'bottom: 30px;';
         css += 'left: 50%;';
-        css += 'margin-left: -80px;';
-        css += 'width: 160px;';
+        css += 'margin-left: -'+carouselCenterWidth/2+'px;';
+        css += 'width: '+carouselCenterWidth+'px;';
+        css += 'font-size: 1em;';
     }else{
+        css += 'bottom: 50px;';
         css += 'left:';
         if(position == 'left'){
-            css += '20%;';
+            css += carouselPadding+'%;';
         }else{
-            css += '80%;';
+            css += 100-carouselPadding+'%;';
         }
         
-        css += 'margin-left: -60px;'
-        css += 'width: 120px;';
+        css += 'margin-left: -'+carouselCenterWidth*0.75/2+'px;'
+        css += 'width: '+carouselCenterWidth*0.75+'px;';
+        css += 'font-size: 0.75em;';
     };
     
     if(off){
         seat.morph(offSize + offPos);
-        seat.fade();
+        carouselFades[carouselSeats.indexOf(seat)] = new Effect.Fade(seat);
     }else{
         seat.morph(css);
         if(!seat.visible()){
             seat.appear();
+        }else{
+            var fade = carouselFades[carouselSeats.indexOf(seat)];
+            if(typeof(fade) != 'undefined' && fade.state == 'running'){
+                fade.cancel();
+                
+                var currentOpacity = seat.getStyle('opacity');
+                seat.appear({ from: currentOpacity });
+            };
         };
     };
     
@@ -147,10 +163,32 @@ function moveSeat(seat, position){
     };
 };
 
-function initCarousel(){
+function initCarousel(width, padding){
+    carouselCenterWidth = width;
+    carouselPadding = padding;
+
     carouselSeats = $$('#carousel .seat');
     currentCarouselPosition = null;
     newCarouselPosition = 1;
-
+    
+    carouselFades = new Array();
+    
     repositionCarousel();
+    initRotation();
+};
+
+function initRotation(delay){
+    if(typeof(carouselRotationInterval) != 'undefined'){
+        clearInterval(carouselRotationInterval);
+    };
+    
+    if(delay){
+        setTimeout('initRotation()', delay);
+    }else{
+        carouselRotationInterval = setInterval("rotateCarousel()", 3000);
+    };
+};
+
+function rotateCarousel(){
+    moveCarousel('right', true);
 };
